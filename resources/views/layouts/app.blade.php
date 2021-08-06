b3dcf09d37093ffe8f26f34978c62a0906da5c85
selimreza1 <selimreza9920@gmail.com>
2021-08-06 16:14:49 +0600

    livewire components

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,12 +6,12 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Livewire Project</title>
-    @livewirestyles
+    @livewireStyles
 </head>
 <body>
 
 {{$slot}}
 
-@livewirescripts
+@livewireScripts
 </body>
 </html>
